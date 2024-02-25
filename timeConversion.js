2d3f8268ed991e3f1c1d62f63b8af4a03ff87a8c
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-02-25 13:51:07 +0800

    feat: Algorithms -> Warmup -> Time Conversion on Hacker Rank

---
+++ timeConversion.js
@@ -0,0 +1,110 @@
+/*
+Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Example
+
+
+Return '12:01:00'.
+
+
+Return '00:01:00'.
+
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+string s: a time in  hour format
+Returns
+
+string: the time in  hour format
+Input Format
+
+A single string  that represents a time in -hour clock format (i.e.:  or ).
+
+Constraints
+
+All input times are valid
+Sample Input 0
+
+07:05:45PM
+Sample Output 0
+
+19:05:45
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+    // Write your code here
+    
+    // Split the time provided thru colon
+    let [hours, minutes, seconds] = s.split(':')
+    
+    // Get the modifier besides seconds
+    let modifier = seconds.slice(-2)
+    
+    // Update the seconds to only get the seconds
+    seconds = seconds.slice(0, 2)
+    
+    if(modifier === 'PM'){
+        if(hours != '12'){
+            hours = parseInt(hours) + 12
+        }else{
+            hours = hours
+        }
+    }
+    else if(modifier === 'AM'){
+        if(hours === '12'){
+            hours = '00'
+        }
+    }
+    
+    return `${hours}:${minutes}:${seconds}`
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = timeConversion(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
